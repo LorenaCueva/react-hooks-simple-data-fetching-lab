ca20fcda72552ec3fa16f18313ebdb43299a7569
Lorena Cueva <lorenacueva@gmail.com>
2022-06-20 15:13:28 -0700

    commit tree b62690c570e649e2856355673f3b198452a4daa8

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import {useEffect, useState} from "react";
+
+function App(){
+
+    const [image, setImage] = useState("");
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(obj => setImage(obj.message))
+    },[])
+
+    if(!image){
+        return <p>Loading...</p>
+    }
+    return (
+        <img src={image} alt="A Random Dog"/>
+    );
+}
+
+export default App;
